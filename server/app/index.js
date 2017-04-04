c37eaab248993a13d1aa01269df662ee42f1f4ce
sawra <sawra@uw.edu>
2017-04-04 10:01:19 -0700

    added express-session

--- server/app/index.js
+++ server/app/index.js
@@ -2,6 +2,7 @@
 
 var app = require('express')();
 var path = require('path');
+var session = require('express-session');
 
 // "Enhancing" middleware (does not send response, server-side effects only)
 
@@ -25,5 +26,14 @@ validFrontendRoutes.forEach(function (stateRoute) {
 app.use(require('./statics.middleware'));
 
 app.use(require('./error.middleware'));
+
+
+app.use(session({
+  // this mandatory configuration ensures that session IDs are not predictable
+  secret: 'kennyiscool', // or whatever you like
+  // these options are recommended and reduce session concurrency issues
+  resave: false, //don't resave if the session object hasn't changed
+  saveUninitialized: false // do save session object even if we don't add anything to it
+}));
 
 module.exports = app;
